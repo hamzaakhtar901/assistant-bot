2eff13c27fbf1241b484ee83e2d8aaef86df4cf3
Shaikh Sabih <shaikhmuhammadsabih@gmail.com>
2023-12-08 01:55:50 +0500

    init

---
+++ app/components/LinkBar.js
@@ -0,0 +1,9 @@
+import { VercelIcon, GithubIcon } from "../icons";
+
+const LinkBar = () => (
+  <div className="absolute top-5 hidden w-full justify-between px-5 sm:flex">
+
+  </div>
+);
+
+export default LinkBar;
